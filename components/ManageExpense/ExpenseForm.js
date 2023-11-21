a6387400464735d690cdbacf09e93a0c205211dc
edegan <eduardord324@gmail.com>
2023-11-21 17:17:32 -0300

    Class 146 - 151

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,60 @@
+import { StyleSheet, Text, View } from "react-native";
+import Input from "./Input";
+import { GlobalStyles } from "../../constants/styles";
+
+function ExpenseForm() {
+  function amountChangeHandler() {}
+
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputRow}>
+        <Input
+          style={styles.rowInput}
+          label={"Amount"}
+          textInputConfig={{
+            KeyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label={"Date"}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
+      <Input
+        label={"Description"}
+        textInputConfig={{
+          multiline: true,
+        }}
+      />
+    </View>
+  );
+}
+
+export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
